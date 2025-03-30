4dbcfd455beac775e0f7ac898c054bbebf4a2138
snighdha09 <79187451+snighdha09@users.noreply.github.com>
2025-03-30 00:57:08 -0400

    Add files via upload

---
+++ Even_odd.py
@@ -0,0 +1,6 @@
+a= int(input("Enter a number: "))
+if a%2==0:
+    print(a," is a even number")
+
+else:
+    print(a," is a odd number")
---
+++ Integer_loop.py
@@ -0,0 +1,4 @@
+res =0
+for i in range(1,51):
+    res=i+res
+print ("The sum of numbers from 1 to 50 is: ", res)
